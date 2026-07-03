78df39c14bbd54c8102560dfd8f4a5a6847a2beb
Nousad <nousad.cat@gmail.com>
2026-07-03 02:20:43 +0200

    patch: alternative buffer & invisible cursor

--- main.py
+++ main.py
@@ -21,29 +21,34 @@ async def main() -> None:
 
     print(f"[+] Loaded {len(data)} lines")
 
-    # Initial spacing for terminal rendering
-    print("\n" * lines_per_frame)
-
-    for frame_start in range(0, len(data), lines_per_frame):
-        frame_block: list[str] = data[frame_start : frame_start + lines_per_frame]
-        delay_line: str = frame_block[0]
-        frame_lines: list[str] = frame_block[1:]
-
-        # Clear terminal (more standard sequence)
-        print("\033[2J\033[H", end="")
-        print("\n".join(frame_lines))
-
-        try:
-            delay_ms: int = int(delay_line)
-
-            await asyncio.sleep(delay_ms * 67 / 1000)
-        except ValueError:
-            # delay_ms:int  = 0
-            continue
-
-
-if __name__ == "__main__":
     try:
-        asyncio.run(main())
+        # Switch to a different buffer, make cursor invisible
+        print("\033[?1049h\033[?25l", end="")
+
+        # Initial spacing for terminal rendering
+        print("\n" * lines_per_frame)
+
+        for frame_start in range(0, len(data), lines_per_frame):
+            frame_block: list[str] = data[frame_start : frame_start + lines_per_frame]
+            delay_line: str = frame_block[0]
+            frame_lines: list[str] = frame_block[1:]
+
+            # Clear terminal
+            print("\033[2J\033[H", end="")
+            print("\n".join(frame_lines))
+
+            try:
+                delay_ms: int = int(delay_line)
+
+                await asyncio.sleep(delay_ms * 67 / 1000)
+            except ValueError:
+                # delay_ms:int  = 0
+                continue
     except KeyboardInterrupt:
         pass
+    finally:
+        # Leave the buffer, make cursor visible again
+        print("\033[?1049l\033[?25h", end="")
+
+if __name__ == "__main__":
+    asyncio.run(main())
